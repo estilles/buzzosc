ad5c9bbea787ddee6d8ebb4d7bc8c2dd8f1a7f19
Johnny Estilles <johnny@estilles.com>
2021-09-22 07:15:25 +0800

    initial version
    Signed-off-by: Johnny Estilles <johnny@estilles.com>

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # buzzosc
 OSC interface for buzzin.live
+
+## OSC Commands
+/buzzosc/start
+/buzzosc/pause
+/buzzosc/reset
+/buzzosc/clear-buzzers
+/buzzosc/toggle-lock
--- src/main.js
+++ src/main.js
@@ -1,46 +1,76 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const osc = require("osc");
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+const BUZZIN_START = "/buzzosc/start";
+const BUZZIN_PAUSE = "/buzzosc/pause";
+const BUZZIN_RESET = "/buzzosc/reset";
+const BUZZIN_CLEAR_BUZZERS = "/buzzosc/clear-buzzers";
+const BUZZIN_TOGGLE_LOCK = "/buzzosc/toggle-lock";
+
+const oscPayload = {
+  [BUZZIN_START]: { id: "timerStart" },
+  [BUZZIN_PAUSE]: { id: "timerPause" },
+  [BUZZIN_RESET]: { id: "timerReset" },
+  [BUZZIN_CLEAR_BUZZERS]: { id: "resetAll" },
+  [BUZZIN_TOGGLE_LOCK]: { id: "toggleLock" },
+};
+
+let win;
+
+let udpPort = new osc.UDPPort({
+  localAddress: "0.0.0.0",
+  localPort: 57121,
+  metadata: true,
+});
+
+udpPort.on("error", (err) => {
+  console.log("OSC: an error occurred: ", err.message);
+});
+
+udpPort.on("message", (msg) => {
+  if (Object.keys(oscPayload).includes(msg.address)) {
+    win.webContents.send("osc", oscPayload[msg.address]);
+  }
+});
+
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: `${__dirname}/renderer.js`,
+    },
+    devTools: false,
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+  win.loadURL("https://buzzin.live/");
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // win.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", () => {
+  udpPort.open();
+  createWindow();
+});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("before-quit", () => {
+  udpPort = null;
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
